4605ee19baba36024674303ff0d13d86cd01beb1
natthaporn <naththaphrnh@gmail.com>
2026-09-23 15:58:39 +0700

    Creta Home

---
+++ src/Hero/Hero.js
@@ -0,0 +1,68 @@
+import "./Hero.css";
+
+
+function Hero(){
+
+    return(
+
+        <section className="hero">
+
+
+            <div className="hero-text">
+
+
+                <p className="hello">
+                    Hi, I'm
+                </p>
+
+
+                <h1>
+                    Nattaporn
+                    <br/>
+                    Wangsuk
+                </h1>
+
+
+                <h3>
+                    Computer Engineering Student
+                </h3>
+
+
+                <p>
+                    Interested in UX/UI Design,
+                    <br/>
+                    Frontend Development
+                    <br/>
+                    and Web Development
+                </p>
+
+
+                <button>
+                    Explore My Journey →
+                </button>
+
+
+            </div>
+
+
+
+            <div className="hero-image">
+
+                <div className="circle">
+
+                </div>
+
+
+            </div>
+
+
+
+        </section>
+
+
+    )
+
+}
+
+
+export default Hero;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import Hero from "../Hero/Hero.js";
+
+
+function Home(){
+
+    return(
+
+        <Hero/>
+
+    )
+
+}
+
+
+export default Home;
